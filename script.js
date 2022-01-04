11d353f1b5667757cb6385fb8ec62bfa89b18b66
DESKTOP-5NBJUSA\bradl <bradley.bieselin@gmail.com>
2022-01-04 12:10:36 -0500

    btc price live updates with $ sign

--- script.js
+++ script.js
@@ -1,76 +1,64 @@
-const trashContainer = document.querySelector('.trash-container');
-const moneyElem = document.querySelector('.money');
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let stockPriceElement = document.getElementById('stock-price');
+const trashContainer = document.querySelector(".trash-container");
+const moneyElem = document.querySelector(".money");
 const currencyFormatter = new Intl.NumberFormat("en-us", {
-    style: "currency",
-    currency: "USD",
-    maximumFractionDigits: 0
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 2
 })
 
 const trashFormatter = new Intl.NumberFormat("en-us", {
-    minimumIntegerDigits: 7,
-    maximumFractionDigits: 0,
-    useGrouping: false
+  minimumIntegerDigits: 7,
+  maximumFractionDigits: 0,
+  useGrouping: false
 })
 
-const ONE_MILLION_DOLLARS = 1000000
+const ONE_MILLION_DOLLARS = 1000000;
 
-setupTrash();
+setupTrash()
 
-async function setupTrash() {
-    const bitcoinPrice = await fetch("https://tscache.com/donation_total.json")
-    .then(res => res.json())
-    .then(data => data.count)
-    moneyElem.innerText = currencyFormatter.format(currentPrice)
+async function setupTrash () {
+  ws.onmessage = (event) => {
+    let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(2);
+    stockPriceElement.innerText = currencyFormatter.format(price);
+  }
 
-    const amountLeft = Math.max(ONE_MILLION_DOLLARS - currentPrice, 0)
-    const stringifiedAmount = trashFormatter.format(amountLeft)
-    const trashAmount = {
-        xxl: {
-            amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
-            icon: "bitcoin"
-        },
-        xl: {
-            amount: parseInt(stringifiedAmount[2]),
-            icon: "bitcoin"
-        },
-        lg: {
-            amount: parseInt(stringifiedAmount[3]),
-            icon: "bitcoin"
-        },
-        md: {
-            amount: parseInt(stringifiedAmount[4]),
-            icon: "bitcoin"
-        },
-        sm: {
-            amount: parseInt(stringifiedAmount[5]),
-            icon: "bitcoin"
-        },
-        xs: {
-            amount: parseInt(stringifiedAmount[6]),
-            icon: "bitcoin"
-        },
+  const trashAmount = {
+    xxl: {
+      amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
+      icon: "bitcoin",
+    },
+    xl: {
+      amount: parseInt(stringifiedAmount[2]),
+      icon: "bitcoin",
+    },
+    lg: {
+      amount: parseInt(stringifiedAmount[3]),
+      icon: "bitcoin",
+    },
+    md: {
+      amount: parseInt(stringifiedAmount[4]),
+      icon: "bitcoin",
+    },
+    sm: {
+      amount: parseInt(stringifiedAmount[5]),
+      icon: "bitcoin",
+    },
+    xs: {
+      amount: parseInt(stringifiedAmount[6]),
+      icon: "bitcoin",
+    },
+  }
+
+  Object.values(trashAmount).forEach(({ amount, icon }) => {
+    for (let i = 0; i < amount; i++) {
+      createTrash(icon)
     }
-    Object.values(trashAmount).forEach(({amount, icon}) => {
-        for(let i = 0; i < amount; i++) {
-            createTrash(icon)
-        }
-    })
+  })
 }
 
 function createTrash(icon) {
-    const img = document.createElement("img")
-    const top = randomNumberBetween(0, 50)
-    const size = top / 5 + 1
-    img.classList.add("trash")
-    img.src = '/imgs/${icon}.svg'
-    img.style.width = `${size}vmin`
-    img.style.height = `${size}vmin`
-    img.style.top = `${top}vh`
-    img.style.left = `${randomNumberBetween(0, 100)}vw`
-    img.style.setProperty("--rotation", `${randomNumberBetween(-30, 30)}deg`)
-    trashContainer.appendChild(img)
-}
-
-function randomNumberBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  
 }
